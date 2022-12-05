fabd03dcc40e02abfcecfefc3f87ce9c44eab706
Taiye Ajimati <taiyeajimati@gmail.com>
2022-12-05 13:39:47 +0400

    First commit

---
+++ restcont/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App ">
+      {/* <header className="App-header">      
+      </header> */}
+<h1>Hello</h1>
+
+    </div>
+  );
+}
+
+export default App;
