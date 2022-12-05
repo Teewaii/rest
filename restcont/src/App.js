37372efd34a63fa5057e11e000ff932fbf0d8879
Taiye Ajimati <taiyeajimati@gmail.com>
2022-12-05 13:45:48 +0400

    Second commit

--- restcont/src/App.js
+++ restcont/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App ">
       {/* <header className="App-header">      
       </header> */}
-<h1>Hello</h1>
+<h1 className='text-3xl'>Hello</h1>
 
     </div>
   );
